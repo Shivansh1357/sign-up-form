d64c5d133d1d15b51e9e1f5e016bd088698a61d2
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-17 14:43:12 +0530

    try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
-function App() {
+const App=()=>{
+  let [name,setName]=useState("");
+  let [email,setEmail]=useState("");
+  let [password,setPassword]=useState("");
+  let [conPassword,setConPassword]=useState("");
+  let [message,setMessage]=useState("");
+  let [success,setSuccess]=useState(false);
+
+  function validation(){
+    if(name=="" || email=="" || password=="" || conPassword==""){
+      setMessage("Error : All the fields are mandatory")
+      setSuccess(false)
+    }
+    else if(password!==conPassword){
+      setMessage("Password mismatched")
+      setSuccess(false)
+    }
+    else{
+      setMessage("Successfully Signed Up!")
+      setSuccess(true)
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    <div className="app">
+        <h1>SignUp</h1>
+        <div id="input">
+        <label for="name">Full Name</label>
+        <input type="text" id="name" onChange={(e)=>setName(e.target.value)}/>
+        </div>
+        <div id="input">
+        <label for="Email">Email</label>
+        <input type="email" id="email" onChange={(e)=>setEmail(e.target.value)}/>
+        </div>
+        <div id="input">
+        <label for="Password">Password</label>
+        <input type="password" id="password" onChange={(e)=>setPassword(e.target.value)}/>
+        </div>
+        <div id="input">
+        <label for="Confirm-password">Confirm Password</label>
+        <input type="password" id="con-password" onChange={(e)=>setConPassword(e.target.value)}/>
+        </div>
+        <p style={{color:success?'green':'red'}}>{message}</p>
+        <button onClick={validation}>Signup</button>
+    </div>
+  )
+}
+export default App
